7dde7131805b6ac22a084b113eb9f01e33839b66
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-06-19 20:02:13 -0400

    Table component created

--- src/Pages/SearchPage/SearchPage.js
+++ src/Pages/SearchPage/SearchPage.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {Form} from '../../Components/Form/form';
-import {Query} from '../../Components/Query/query';
-const tableHeaders = require('../../Utils/tableHeaders');
+import {Table} from '../../Components/Table/table';
 
 export const SearchPage = () => {
 
@@ -12,7 +11,8 @@ export const SearchPage = () => {
     
     const [result, setResult] = useState({
         tableState: query.searchType,
-        items: []
+        items: [],
+        error: ''
     })
 
     const handleFormChange = (input) => {
@@ -27,12 +27,21 @@ export const SearchPage = () => {
     const handleFormSubmit = () => {
         fetch(`https://api.github.com/search/${query.searchType}?q=${encodeURIComponent(query.query)}`)
         .then(res => res.json())
-        .then(data => {  
+        .then(data => {
+            if(data.errors){
+                return setResult(prevState => {
+                    return {
+                        ...prevState,
+                        error: data.errors[0].message
+                    }
+                })                
+            }
             setResult((prevState) => {
                 return {
                     ...prevState,
                     tableState: query.searchType,
-                    items: data.items 
+                    items: data.items,
+                    error: ''
                 }
             })       
         })
@@ -41,24 +50,7 @@ export const SearchPage = () => {
     return(
         <div>
             <Form userInput={query} onFormChange={handleFormChange} onFormSubmit={handleFormSubmit} />
-            <table>
-                <thead>
-                    <tr>
-                        <th>{tableHeaders[result.tableState]["author"]}</th>
-                        <th>{tableHeaders[result.tableState]["repo"]}</th>
-                        <th>{tableHeaders[result.tableState]["url"]}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {result.items.map(item => {
-                        console.log(item)
-                        if(result.tableState === "code") return <Query key={item.sha} author={item.repository.owner.login} repos={item.repository.name} url={item.html_url}/>                        
-                        else if (result.tableState === "issues") return <Query key={item.id} author={item.user.login} repos={item.title} url={item.html_url} />
-                        else if (result.tableState === "repositories") return <Query key={item.id} author={item.owner.login} repos={item.name} url={item.html_url} />
-                        else return <Query key={item.id} author={item.login} repos={item.type} url={item.html_url} />
-                    })}
-                </tbody>
-            </table>            
+            <Table tableState={result.tableState} items={result.items} error={result.error} />         
         </div>
     )
 }
